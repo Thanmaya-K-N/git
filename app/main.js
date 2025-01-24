6374451af756a2f36f2d8f9c0afc2c10b587a190
Thanmaya-K-N <nm8223900@gmail.com>
2025-01-24 23:28:48 +0530

    read a blob object

--- app/main.js
+++ app/main.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
 const path = require("path");
+const zlib = require("zlib");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
-console.error("Logs from your program will appear here!");
+console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const command = process.argv[2];
@@ -10,6 +11,10 @@ const command = process.argv[2];
 switch (command) {
   case "init":
     createGitDirectory();
+    break;
+  case "cat-file":
+    const hash = process.argv[4];
+    catFile(hash);
     break;
   default:
     throw new Error(`Unknown command ${command}`);
@@ -22,4 +27,11 @@ function createGitDirectory() {
 
   fs.writeFileSync(path.join(process.cwd(), ".git", "HEAD"), "ref: refs/heads/main\n");
   console.log("Initialized git directory");
+}
+
+async function catFile(hash){
+    const content = await fs.readFileSync(path.join(process.cwd(), ".git", "objects", hash.slice(0,2), hash.slice(2)));
+    const dataUnzipped = zlib.inflateSync(content);
+    const res = dataUnzipped.toString().split('\0')[1];
+    process.stdout.write(res);
 }
